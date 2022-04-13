a8d47dc3e54a951bc06942b5495570d491d29540
P4trick18 <100170031+P4trick18@users.noreply.github.com>
2022-04-12 22:28:15 -0300

    Add files via upload

---
+++ Trabalho.js
@@ -0,0 +1,48 @@
+var varString = 'aluno';
+ var varBoolean = false;
+var varNumber = 0;
+
+var varArray = ['Leandro', 'Andre', 'Newton', 'Elias', 'Fernanda'];
+
+var aluno = {
+    nome: "Patrick",
+    idade: 17,
+
+};
+
+var numero1 = 16;
+var numero2 = 18;
+
+var resultado = numero1 + numero2;
+
+alert(resultado)
+
+let coelho = true
+if (coelho === false) {
+   var bom = 14;
+}else{
+    var ruim = 7;
+}
+
+alert(coelho);
+
+function Professores() {
+    var varArray = ['Julio Cocielo', 'Games Eduu', 'Viniccius13', 'Davi', 'Player94'];
+    
+}
+
+function IMC() {
+    var nome = document.getElementById("Nome").value;
+    var peso = document.getElementById("Peso").value;
+    var altura = document.getElementById("Altura").value;
+    var resultado = document.getElementById("Resultado").value;
+
+//imc
+var imc = peso / (altura * alturA);
+
+if(imc <17)
+{
+    resultado.value = nome + "voce esta abaixo do peso";
+}
+else if(imc >17 && imc <=18.49);
+} 
